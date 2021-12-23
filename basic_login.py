d9cb05a2fdac71986b0fa981dea01a3b8ba2a8b7
YamiSagi <84624853+yamisagi@users.noreply.github.com>
2021-12-23 22:17:18 +0300

    Add files via upload

---
+++ basic_login.py
@@ -0,0 +1,18 @@
+while True:
+    x = input("Hello what is your Username dude ?>> ")
+    if x != "yamisagi":
+        print("Your are not my boi. Try again")
+        continue
+    else: 
+        pass
+    break
+
+while True:
+    print("Hai my boi what is your passssssssss ? (You kno dat)")
+    password = input("Ha ? : ")
+    if password == "maboi123":
+        break
+    else:
+       print("again")
+       continue
+print("Welcome ma boiii")
